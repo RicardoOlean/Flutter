a1c1a023acb21feeca74ab8c94b3ac7909eb6158
RICARDO OLIVEIRA LEANDRO <rick.lean2018@gmail.com>
2026-04-09 10:00:32 -0300

    Inicio da API

---
+++ Atvidade_Avaliativa/app_tarefas/lib/pages/telaget.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class Telaget extends StatefulWidget {
+  const Telaget({super.key});
+
+  @override
+  State<Telaget> createState() => _TelagetState();
+}
+
+class _TelagetState extends State<Telaget> {
+  String resultado = ''; // Variavel para receber a tarefa
+
+// meu ip 10.109.72.16
+void fagerGet () async {
+
+
+}
+
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
